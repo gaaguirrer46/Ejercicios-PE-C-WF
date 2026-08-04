174dbf1eaa27cf70052d2ea25403fa8243f0c72b
Guillermo Aguirre <Ing.gaaguirrer@outlook.es>
2026-08-04 10:09:20 -0600

    ej4

---
+++ Ejercicios top down/Sin validación/Ejercicio 04/Ejercicio 04/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtNum1.Text);
+            double n2 = double.Parse(txtNum2.Text);
+            lblSuma.Text = (n1 + n2).ToString();
+            lblResta.Text = (n1 - n2).ToString();
+            lblMultiplicacion.Text = (n1 * n2).ToString();
+            lblDivision.Text = (n1 / n2).ToString();
+
+        }
+    }
+}
