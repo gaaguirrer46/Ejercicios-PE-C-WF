4ebfdcfa1d21d83d0c2b68143e6849952225d651
Guillermo Aguirre <Ing.gaaguirrer@outlook.es>
2026-08-04 10:20:07 -0600

    ej4 validado

---
+++ Ejercicios top down/Validados/Ejercicio 04/Ejercicio 04/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Ejercicio_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblMultiplicacion = new System.Windows.Forms.Label();
+            this.lblResta = new System.Windows.Forms.Label();
+            this.lblSuma = new System.Windows.Forms.Label();
+            this.lblDivision = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Suma";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Multiplicación";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "División";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(126, 34);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 6;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(126, 73);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 7;
+            // 
+            // lblMultiplicacion
+            // 
+            this.lblMultiplicacion.AutoSize = true;
+            this.lblMultiplicacion.Location = new System.Drawing.Point(131, 242);
+            this.lblMultiplicacion.Name = "lblMultiplicacion";
+            this.lblMultiplicacion.Size = new System.Drawing.Size(0, 16);
+            this.lblMultiplicacion.TabIndex = 8;
+            // 
+            // lblResta
+            // 
+            this.lblResta.AutoSize = true;
+            this.lblResta.Location = new System.Drawing.Point(131, 210);
+            this.lblResta.Name = "lblResta";
+            this.lblResta.Size = new System.Drawing.Size(0, 16);
+            this.lblResta.TabIndex = 9;
+            // 
+            // lblSuma
+            // 
+            this.lblSuma.AutoSize = true;
+            this.lblSuma.Location = new System.Drawing.Point(131, 183);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(0, 16);
+            this.lblSuma.TabIndex = 10;
+            // 
+            // lblDivision
+            // 
+            this.lblDivision.AutoSize = true;
+            this.lblDivision.Location = new System.Drawing.Point(131, 276);
+            this.lblDivision.Name = "lblDivision";
+            this.lblDivision.Size = new System.Drawing.Size(0, 16);
+            this.lblDivision.TabIndex = 11;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(40, 135);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 12;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 398);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblDivision);
+            this.Controls.Add(this.lblSuma);
+            this.Controls.Add(this.lblResta);
+            this.Controls.Add(this.lblMultiplicacion);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Operaciones Aritméticas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblMultiplicacion;
+        private System.Windows.Forms.Label lblResta;
+        private System.Windows.Forms.Label lblSuma;
+        private System.Windows.Forms.Label lblDivision;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Ejercicios top down/Validados/Ejercicio 04/Ejercicio 04/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Variable para saber qué campo causó el error
+            TextBox campoConError = null;
+
+            try
+            {
+                // Validación de obligatoriedad
+                if (string.IsNullOrWhiteSpace(txtNum1.Text))
+                {
+                    campoConError = txtNum1;
+                    throw new ArgumentException("El número 1 no puede estar vacío.");
+                }
+                if (string.IsNullOrWhiteSpace(txtNum2.Text))
+                {
+                    campoConError = txtNum2;
+                    throw new ArgumentException("El número 2 no puede estar vacío.");
+                }
+
+                // Validación de tipo de dato
+                if (!double.TryParse(txtNum1.Text, out double n1))
+                {
+                    campoConError = txtNum1;
+                    throw new ArgumentException("El número 1 debe ser un número decimal (usa punto).");
+                }
+                if (!double.TryParse(txtNum2.Text, out double n2))
+                {
+                    campoConError = txtNum2;
+                    throw new ArgumentException("El número 2 debe ser un número decimal (usa punto).");
+                }
+
+                // LO NUEVO: Validación de división por cero (Técnica 6 - Reglas de negocio)
+                if (n2 == 0)
+                {
+                    campoConError = txtNum2;  // El error está en el segundo número
+                    throw new ArgumentException("No se puede dividir entre cero.");
+                }
+
+                // Si llegamos aquí, todo es válido
+                double suma = n1 + n2;
+                double resta = n1 - n2;
+                double multiplicacion = n1 * n2;
+                double division = n1 / n2;
+
+                lblSuma.Text = suma.ToString("F2");
+                lblResta.Text = resta.ToString("F2");
+                lblMultiplicacion.Text = multiplicacion.ToString("F2");
+                lblDivision.Text = division.ToString("F2");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show("DATO INCORRECTO: " + ex.Message,
+                                "Validación fallida",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                // LO NUEVO: Enfocamos el campo que causó el error
+                if (campoConError != null)
+                {
+                    // Borramos el contenido para que el usuario empiece de cero
+                    campoConError.Text = "";
+                    campoConError.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR INESPERADO: " + ex.Message,
+                                "Error crítico",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+
+                // Limpiamos todos los labels por seguridad
+                lblSuma.Text = "";
+                lblResta.Text = "";
+                lblMultiplicacion.Text = "";
+                lblDivision.Text = "";
+            }
+
+        }
+    }
+}
