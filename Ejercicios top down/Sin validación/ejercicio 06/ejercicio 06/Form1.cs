603846e77f12fe50489b542ea1656e6f1bebb62b
Guillermo Aguirre <Ing.gaaguirrer@outlook.es>
2026-08-04 13:47:36 -0600

    ej 6

---
+++ Ejercicios top down/Sin validación/ejercicio 06/ejercicio 06/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtNum1.Text);
+            double n2 = double.Parse(txtNum2.Text);
+            double n3 = double.Parse(txtNum3.Text);
+            double promedio = (n1 + n2 + n3) / 3;
+            lblPromedio.Text = promedio.ToString("F2");
+        }
+    }
+}
